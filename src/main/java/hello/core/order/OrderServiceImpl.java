abc12d46c7fa3926ad5036dca62bfb0ae0b0bedb
janghyeon <amapow25@gmail.com>
2023-01-28 12:48:27 +0900

    Spring Bean Appconfig.xml 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,8 +1,8 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.*;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 

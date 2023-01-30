09c56516208dfb713f18175e806c60cf63a7ecf5
janghyeon.jung <amapow25@gmail.com>
2023-01-30 14:16:39 +0900

    Dipendency Injection Lombok 적용

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setAge(10);
+        helloLombok.setName("name");
+
+        helloLombok.getAge();
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}

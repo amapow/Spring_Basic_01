ce9850221dd1c724d2d426c43401633f0217f777
janghyeon <amapow25@gmail.com>
2023-01-31 09:56:52 +0900

    Bean Scope 싱글톤 스코프, 프로토타입 스코프

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PrototypeTest {
+
+
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+        System.out.println("find prototypeBean1");
+        ProtoTypeBean protoTypeBean1 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("find prototypeBean2");
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("protoTypeBean1 = " + protoTypeBean1);
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+        ac.close();
+        assertThat(protoTypeBean1).isNotSameAs(protoTypeBean2);
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("ProtoTypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("ProtoTypeBean.destroy");
+        }
+    }
+
+}
